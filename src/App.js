fb8424ca01f2290949027ddff5217bdcf58ae0a7
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-08-23 00:01:26 +0530

    Just created a Guess and Roll Game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const number = [1, 2, 3, 4, 5, 6];
 
 function App() {
+  const maxLimit = 10;
+  const [chosenNumber, setChosenNumber] = useState(null);
+  const [rolledNumber, setRolledNumber] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [msg, setMsg] = useState(null);
+  const [wins, setWins] = useState(0);
+  const [loss, setLoss] = useState(0);
+  const [selectedNumber, setSelectedNumber] = useState(null);
+  const [limit, setLimit] = useState(0);
+
+  const getChosenNumber = (event, num) => {
+    const selectedNumber = parseInt(event.target.textContent);
+    setChosenNumber(selectedNumber);
+    setMsg(null);
+    setSelectedNumber(num);
+  };
+
+  const rollDice = () => {
+    const randomIndex = Math.floor(Math.random() * number.length);
+    const rolledValue = number[randomIndex];
+    setRolledNumber(rolledValue);
+    setLimit(limit + 1);
+  };
+
+  const checkWinning = () => {
+    if (chosenNumber === rolledNumber) {
+      setStatus("You Guessed right✅");
+      setWins(wins + 1);
+    } else {
+      setStatus("You are wrong ❌");
+      // setRolledNumber(null);
+      setChosenNumber(null);
+      setLoss(loss + 1);
+      setMsg("Choose again !🫤");
+    }
+  };
+
+  useEffect(() => {
+    if (rolledNumber !== null) {
+      checkWinning();
+    }
+  }, [rolledNumber]);
+
+  const resetAll = () => {
+    setChosenNumber(null);
+    setLimit(0);
+    setRolledNumber(null);
+    setLoss(0);
+    setWins(0);
+    setSelectedNumber(null);
+    setStatus(null);
+    // set
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <>
+      <div className="heading">
+        <h1>Guess & Roll</h1>
+      </div>
+      <div className="Score">
+        <h2>
+          <span className="win">Win = {wins}</span>,{" "}
+          <span className="loss">Loss = {loss}</span>
+        </h2>
+      </div>
+      <div className="main">
+        <div className="left">
+          <div className="details">
+            <h2>Choose your number first</h2>
+            <div className="ButtonSelector">
+              {number.map((num) => (
+                <button
+                  key={num}
+                  onClick={(event) => getChosenNumber(event, num)}
+                  className={selectedNumber === num ? "selected" : ""}
+                  disabled={limit === maxLimit}
+                >
+                  {num}
+                </button>
+              ))}
+            </div>
+            <p>Total tries left: {maxLimit - limit}</p>
+            {chosenNumber !== null && <h3>You have chosen: {chosenNumber}</h3>}
+            <h2>{msg}</h2>
+          </div>
+        </div>
+
+        <div className="right">
+          <div className="App">
+            <div className="Container">
+              <span className="text">
+                {rolledNumber !== null ? rolledNumber : "Start"}
+              </span>
+            </div>
+          </div>
+          <div className="roller">
+            <button
+              onClick={rollDice}
+              disabled={chosenNumber === null || limit >= maxLimit}
+            >
+              Roll
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1 className="result">{status}</h1>
+        <div id="retry">
+          {limit >= maxLimit && <button onClick={resetAll}>Replay 🔁</button>}
+        </div>
+      </div>
+      <div className="owner">
+        {" "}
+        Powered by -{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://connectit000.000webhostapp.com/himanshu/"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Himanshu Prajapati
         </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
